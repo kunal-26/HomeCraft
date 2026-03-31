261aaeca6507aedb7d71af6f386d6efc66e8946d
kunal jain <55075272+kunal-26@users.noreply.github.com>
2026-03-31 23:47:04 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,16 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDmlWpe3UInANOIijzkE0AKSaUf9jUj8I4",
-    authDomain: "homecraft-88329.firebaseapp.com",
-    projectId: "homecraft-88329",
-    storageBucket: "homecraft-88329.firebasestorage.app",
-    messagingSenderId: "1057372655076",
-    appId: "1:1057372655076:web:c51fb70733ba56d1114bf1",
-    measurementId: "G-JJ4RLQ093J"
+  apiKey: "AIzaSyDmlWpe3UInANOIijzkE0AKSaUf9jUj8I4",
+  authDomain: "homecraft-88329.firebaseapp.com",
+  projectId: "homecraft-88329",
+  storageBucket: "homecraft-88329.appspot.com",
+  messagingSenderId: "1057372655076",
+  appId: "1:1057372655076:web:c51fb70733ba56d1114bf1",
+  measurementId: "G-JJ4RLQ093J"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const storage = firebase.storage();
+
+console.log("✅ Firebase Connected");
